506b7df21d2059dd8fbb7865652b93e663cdad87
Timofey <sukonkin.timofei@gmail.com>
2024-09-04 15:32:10 +0300

    added gitignore lessons

--- 03_custom_modules/output1.js
+++
@@ -1,7 +0,0 @@
-import * as data from './data.js';
-import { message, number, person, changePersonName } from './data.js';
-
-changePersonName('Jack');
-
-console.log('OUTPUT 1', data);
-console.log('OUTPUT 1', message, number, person);
